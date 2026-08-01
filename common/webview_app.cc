92336be74e54bfbcd996895f706e9a0b1c358e8d
Minesh Purohit <mineshp@mechasystems.com>
2026-08-01 14:49:09 +0530

    fix: hide scrollbar

--- common/webview_app.cc
+++ common/webview_app.cc
@@ -117,6 +117,7 @@ void WebviewApp::OnBeforeCommandLineProcessing(const CefString &process_type, Ce
         command_line->AppendSwitchWithValue("user-agent-product", "mechanix-browser/133");
         command_line->AppendSwitchWithValue("lang", "en-US");
         command_line->AppendSwitch("no-sandbox");
+        command_line->AppendSwitch("hide-scrollbars");
 
         // Platform & Graphics
         command_line->AppendSwitchWithValue("ozone-platform", "wayland");
